5eea5d7edb3924f306b7afe5a593eaa30dfec450
Rui Xue <rayalex260@gmail.com>
2025-02-23 23:02:41 -0600

    Added camera and backgrouhd with improved attack animation

--- Flower-Frenzy-main/coco.gd
+++ Flower-Frenzy-main/coco.gd
@@ -5,8 +5,10 @@ extends CharacterBody2D
 @export_range(0,1) var accerlation = 0.1
 @export_range(0,1) var decelration = 0.1
 
-@export var jump_force =-400.0
+@export var jump_force = -400.0
 @export_range(0,1) var decelerate_on_jump_release = 0.5
+
+var is_attacking = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,40 +17,51 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		$AnimatedSprite2D.animation = "jump"
 		velocity.y = jump_force
 		
-		
 	if Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y *=  decelerate_on_jump_release
-		
-	var speed
-	if Input.is_action_pressed("sprint"):
-		speed = run_speed
-	else:
-		speed = walk_speed
+		velocity.y *= decelerate_on_jump_release
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_h = direction < 0
-		velocity.x = move_toward(velocity.x, direction * speed, speed * accerlation)
-	else:
-		velocity.x = move_toward(velocity.x, 0, walk_speed * decelration)
-
-	if $AnimatedSprite2D.animation != "attack" and velocity.length() == 0:
-		$AnimatedSprite2D.animation = "still"
-		
-	if Input.is_action_just_pressed("attack"):
+	# Handle attack
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		is_attacking = true
 		$AnimatedSprite2D.animation = "attack"
-		
+		$AnimatedSprite2D.play()
+		return  # Prevent other animations from playing during attack
+
+	# Handle movement only if not attacking
+	if not is_attacking:
+		var speed
+		if Input.is_action_pressed("sprint"):
+			speed = run_speed
+		else:
+			speed = walk_speed
+
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = move_toward(velocity.x, direction * speed, speed * accerlation)
+			$AnimatedSprite2D.flip_h = direction < 0
+			if is_on_floor():
+				$AnimatedSprite2D.animation = "walk"
+		else:
+			velocity.x = move_toward(velocity.x, 0, walk_speed * decelration)
+
+		# Jump animation
+		if not is_on_floor():
+			$AnimatedSprite2D.animation = "jump"
+		elif velocity.length() == 0:
+			$AnimatedSprite2D.animation = "still"
+
 	$AnimatedSprite2D.play()
-	
 	move_and_slide()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if $AnimatedSprite2D.animation == "attack":
-		$AnimatedSprite2D.animation = "still"
-		$AnimatedSprite2D.play()
+		is_attacking = false
+		# Resume appropriate animation based on state
+		if not is_on_floor():
+			$AnimatedSprite2D.animation = "jump"
+		elif velocity.length() == 0:
+			$AnimatedSprite2D.animation = "still"
+		else:
+			$AnimatedSprite2D.animation = "walk"
