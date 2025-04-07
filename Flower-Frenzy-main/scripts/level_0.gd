7f5c385a14413274616604b35161d610b6a96693
anna <annakeiotto@gmail.com>
2025-04-07 00:31:34 -0500

    added more flower collectibles

--- Flower-Frenzy-main/scripts/level_0.gd
+++ Flower-Frenzy-main/scripts/level_0.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var character = $coco
 var shoe_scene = preload("res://scenes/converse_monster.tscn")
 var caterpillar_scene = preload("res://scenes/caterpillar_monster.tscn")
+var flower_scene = preload("res://scenes/level_0_flower.tscn")
 
 func _init() -> void:
 	# Update the global level variable before _ready() is called
@@ -25,5 +26,13 @@ func _ready() -> void:
 
 	for marker in shoe_spawn_points:
 		var mob = shoe_scene.instantiate()
+		mob.global_position = marker.global_position
+		add_child(mob)
+		
+	# Get all markers in the scene
+	var flower_spawn_points = get_tree().get_nodes_in_group("flower_spawns")
+
+	for marker in flower_spawn_points:
+		var mob = flower_scene.instantiate()
 		mob.global_position = marker.global_position
 		add_child(mob)
